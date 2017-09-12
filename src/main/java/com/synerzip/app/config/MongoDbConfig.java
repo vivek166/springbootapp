bf2209cc809dd3ef0b4bc6304636cd8c3b831315
vivek166 <vivek.tiwari.cse16@gmail.com>
2017-09-12 14:58:36 +0530

    mongodb configuration with property file

--- src/main/java/com/synerzip/app/config/MongoDbConfig.java
+++ src/main/java/com/synerzip/app/config/MongoDbConfig.java
@@ -1,22 +1,33 @@
 package com.synerzip.app.config;
 
 import java.net.UnknownHostException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.core.MongoTemplate;
+
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class MongoDbConfig {
+
+	@Value("${spring.data.mongodb.host}")
+	private String host;
+	
+	@Value("${spring.data.mongodb.db}")
+	private String db;
 
 	@Bean
 	public Mongo mongo() throws UnknownHostException {
-		return new MongoClient("localhost");
+		return new MongoClient(host);
 	}
 
 	public @Bean
 	MongoTemplate mongoTemplate() throws UnknownHostException {
-		return new MongoTemplate(mongo(), "users");
+		return new MongoTemplate(mongo(), db);
 	}
 }
